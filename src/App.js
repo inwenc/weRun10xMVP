3b2741929700f1db61d83a76486b82b75e7c63a1
yingwen chen <yingwen.chen@balena.io>
2023-07-12 18:40:38 -0600

    map can display uploaded gpx coordinates

--- src/App.js
+++ src/App.js
@@ -3,36 +3,54 @@ import './App.css';
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import MapComponent from "./MapComponent";
-import UploadComponent from './Upload';
 import "leaflet/dist/leaflet.css";
 
 const App = () => {
   //const [gpxPoints, setGpxPoints] = useState([]);
   const coordinates = [40.7826, -73.968285]; // Example coordinates
 
-  // useEffect(() => {
-  //   // Fetch the parsed GPX data from the backend
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await axios.get("/parsed-gpx");
-  //       const { points } = response.data;
-  //       setGpxPoints(points);
-  //     } catch (error) {
-  //       console.error("Error occurred while fetching GPX data", error);
-  //     }
-  //   };
+ const [selectedFile, setSelectedFile] = useState(null);
+ const [responsePoints, setResponsePoints] = useState(0);
 
-  //   fetchData();
-  // }, []);
+ const handleFileChange = (event) => {
+   setSelectedFile(event.target.files[0]);
+ };
+
+ const handleFileUpload = () => {
+   if (selectedFile) {
+     const formData = new FormData();
+     formData.append("file", selectedFile);
+
+     fetch("http://localhost:3001/upload", {
+       method: "POST",
+       body: formData,
+     })
+       .then((response) => response.json())
+       .then((data) => {
+         const dataOne = data.points.map((point) => [point.lat, point.lon]);
+         setResponsePoints(dataOne);
+         // onUploadComplete(response);
+         console.log("points", dataOne);
+       })
+       .catch((error) => {
+         console.error("Error occurred during upload", error);
+       });
+   }
+ };
 
   return (
     <div className="App">
       <div>
         <h1>OpenStreetMap Example</h1>
-        <MapComponent className="mapComponents" coordinates={coordinates} />
+        <MapComponent
+          className="mapComponents"
+          coordinates={coordinates}
+          trackPoints={responsePoints}
+        />
       </div>
-      <div className='Upload-Container'>
-        <UploadComponent />
+      <div className="Upload-Container">
+        <input type="file" accept=".gpx" onChange={handleFileChange} />
+        <button onClick={handleFileUpload}>Upload GPX</button>
       </div>
     </div>
   );
--- src/MapComponent.js
+++ src/MapComponent.js
@@ -1,59 +1,30 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet";
+import React, { useState, useEffect } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Polyline,
+} from "react-leaflet";
 import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 const MapComponent = ({ coordinates, trackPoints }) => {
-    const customMarker = new L.icon({
-      iconUrl: "https://unpkg.com/leaflet@1.4.0/dist/images/marker-icon.png",
-      iconSize: [25, 41],
-      iconAnchor: [13, 0],
-    });
-    //const points = JSON.parse(trackPoints);
-    // let positions = null;
-    // positions = trackPoints.points? trackPoints.points.map((point) => [point.lat, point.lon])
-    //   : null;
-   // console.log("type", trackPoints);
-   const positions = [
-    [
-        40.776253,
-        -73.964262
-    ],
-    [
-        40.776903,
-        -73.963786
-    ],
-    [
-        40.77694,
-        -73.963928
-    ],
-    [
-        40.777146,
-        -73.963782
-    ],
-    [
-        40.777152,
-        -73.963826
-    ],
-    [
-        40.777294,
-        -73.964097
-    ],
-    [
-        40.777934,
-        -73.965038
-    ],
-    [
-        40.77799,
-        -73.96514
-    ],
-    [
-        40.778081,
-        -73.965373
-    ],
-    [
-        40.778167,
-        -73.965789
-    ]]
+  const customMarker = new L.icon({
+    iconUrl: "https://unpkg.com/leaflet@1.4.0/dist/images/marker-icon.png",
+    iconSize: [25, 41],
+    iconAnchor: [13, 0],
+  });
+ const [currentTrackPoints, setTrackpoints] = useState(null);
+
+ useEffect(() => 
+ {
+    if (trackPoints !== undefined) {
+        setTrackpoints(trackPoints)
+    } else {
+      return;
+    } 
+ },[trackPoints])
 
   return (
     <MapContainer style={{ height: "100vh" }} center={coordinates} zoom={13}>
@@ -61,16 +32,14 @@ const MapComponent = ({ coordinates, trackPoints }) => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-      {(trackPoints !== undefined && trackPoints.length > 0) && (
-        <Polyline positions={positions} fillColor="red" color="blue" weight={3} />
+      {trackPoints && (
+        <Polyline
+          positions={trackPoints}
+          fillColor="red"
+          color="blue"
+          weight={3}
+        />
       )}
-      <Marker position={coordinates} icon={customMarker}>
-        <Popup>
-          A pretty CSS3 popup.
-          <br />
-          Easily customizable.
-        </Popup>
-      </Marker>
     </MapContainer>
   );
 };
